944e9f0845a355b67ffb1eae9d25a873db8e6b51
Vitaly <w.182@mail.ru>
2022-04-07 02:47:43 +0300

    change for deploy

--- next.config.js
+++ next.config.js
@@ -2,8 +2,31 @@
 const nextConfig = {
   reactStrictMode: true,
     images:{
-    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
+    loader: 'imgix',
+    path: ""
   }
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      // '/search': { page: '/search' },
+      // '/about': { page: '/about' },
+      // '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      // '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      // '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+    }
+  },
+}
+
